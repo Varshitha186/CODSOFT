14c741fac54772f12215537a011e4f2982a66c9f
Varshitha186 <varshithashetty006@gmail.com>
2025-08-30 16:46:20 +0530

    Create Task2.java
    Added Task2 - Student Grade Calculator

---
+++ Task2/Task2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int[] marks = new int[subjects];
+        int totalMarks = 0;
+
+        // Input marks
+        for (int i = 0; i < subjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = sc.nextInt();
+            totalMarks += marks[i];
+        }
+
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / subjects;
+
+        // Grade calculation
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 75) {
+            grade = 'B';
+        } else if (averagePercentage >= 50) {
+            grade = 'C';
+        } else if (averagePercentage >= 35) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display results
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
